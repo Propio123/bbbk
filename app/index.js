e0901714b6201853fc655505a05d54d7f615ba7f
Propio123 <juanchavez123@hotmail.es>
2026-05-23 16:20:39 -0500

    Add download link for AppDental.apk
    Added a download link for AppDental.apk in the Page component.

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 import HomeScreen from "../src/screens/Home/HomeScreen";
 
 export default function Page() {
+  <a id="download-link" href="https://docs.google.com/uc?export=download&id=TU_ID_AQUÍ" class="btn-download" download="AppDental.apk">
   return <HomeScreen />;
 }
