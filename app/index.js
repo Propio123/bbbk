9aa798f2bbb84197948618d5b41595efe2962d1f
Propio123 <juanchavez123@hotmail.es>
2026-05-25 21:56:57 -0500

    Update download link in index.js

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import HomeScreen from "../src/screens/Home/HomeScreen";
-const urlDescarga = "https://docs.google.com/uc?export=download&id=TU_ID_AQUÍ";
+const urlDescarga = "https://docs.google.com/uc?export=download&id=1Z3Pm20ZmLHkXMxN5B3I2zF1R9_LqkNEh";
 export default function Page() {
-  <a id="download-link" href="https://docs.google.com/uc?export=download&id=TU_ID_AQUÍ" class="btn-download" download="AppDental.apk">
+  <a id="download-link" href="https://docs.google.com/uc?export=download&id=1Z3Pm20ZmLHkXMxN5B3I2zF1R9_LqkNEh" class="btn-download" download="AppDental.apk">
   return <HomeScreen />;
 }
