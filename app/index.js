d342f57da232148f87f7ac51b96fdab546e75e8c
Propio123 <juanchavez123@hotmail.es>
2026-05-25 21:44:28 -0500

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 import HomeScreen from "../src/screens/Home/HomeScreen";
-
+const urlDescarga = "https://docs.google.com/uc?export=download&id=TU_ID_AQUÍ";
 export default function Page() {
   <a id="download-link" href="https://docs.google.com/uc?export=download&id=TU_ID_AQUÍ" class="btn-download" download="AppDental.apk">
   return <HomeScreen />;
